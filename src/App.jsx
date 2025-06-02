33708085b5b121e86742ed31da5c885477cbb085
Akash <hafejmohammadakash@gmail.com>
2025-06-02 09:53:38 +0600

    navber create kortesi

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,11 @@ function App() {
 
   return (
     <>
+
+    {/* Navber */}
+    <nav>
+      
+    </nav>
     
       <h1 className='text-4xl text-red-500 font-bold'>SPORESSO Emporlum</h1>
      
