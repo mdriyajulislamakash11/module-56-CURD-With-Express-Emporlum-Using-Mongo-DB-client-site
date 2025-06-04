a630a8329e31228733a98d1e1dd633ec80b69c44
Akash <hafejmohammadakash@gmail.com>
2025-06-04 21:54:45 +0600

    user delete korar kaj korlam

--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
@@ -6,8 +6,19 @@ const Users = () => {
   const [users, setUsers] = useState(loadedUsers);
 
   const handleDelet = (_id) => {
-    console.log(_id)
-  }
+    console.log(_id);
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          const remaining = users.filter((user) => user._id !== _id);
+          setUsers(remaining);
+        }
+      });
+  };
 
   return (
     <div className="overflow-x-auto">
@@ -33,7 +44,12 @@ const Users = () => {
               <td>
                 {/* <button className="btn btn-secondary px-6 mx-4">V</button> */}
                 <button className="btn btn-primary px-6 mx-4">E</button>
-                <button onClick={() => handleDelet(user?._id)} className="btn px-6 btn-warning">X</button>
+                <button
+                  onClick={() => handleDelet(user?._id)}
+                  className="btn px-6 btn-warning"
+                >
+                  X
+                </button>
               </td>
             </tr>
           ))}
